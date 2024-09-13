c4dce7b53686bebf739b023afef8086929072802
Neo-Nguyen-95 <dzung.usth@gmail.com>
2024-09-14 00:25:58 +0700

    clean code

--- data_extraction_v1.py
+++ data_extraction_v1.py
@@ -1,27 +1,29 @@
 #%% LIBRARY
 import pdfplumber
 import pandas as pd
+import re
 
 file_path = 'data/sao_ke_MTTQ_p1_3.pdf'
-#%% I. EXTRACT DATE % CASH
+#%%
 def extract_table(file_path):
     with pdfplumber.open(file_path) as pdf:
         all_tables = []
+        all_texts = ''
         for page in pdf.pages:
             tables = page.extract_table()
             all_tables.extend(tables)
             
-    return all_tables
+            all_texts += page.extract_text()
+            
+    return all_tables, all_texts
 
-data1 = extract_table(file_path)
-#%%
-dates = []
-docs = []
-money_texts = []
-moneys = []
+all_tables, all_texts = extract_table(file_path)
 
-for index, content in enumerate(data1):
-    if index%2==1:
+#%% I. EXTRACT DATE & CASH
+dates, docs, money_texts, moneys = [] , [], [], []
+
+for index, content in enumerate(all_tables):
+    if index % 2 == 1:
         dates.extend(content[0].splitlines()[0::2])
         docs.extend(content[0].splitlines()[1::2])
         money_texts.extend(content[2].splitlines())
@@ -31,42 +33,33 @@ for index, content in enumerate(data1):
 if (len(dates)==len(docs)) & (len(docs)==len(moneys)):
     print(f'Number of transaction: {len(dates)}')
     print("Everything's fine")
-#####------- DONE SO FAR
+
 #%% II. EXTRACT CONTENT
-def extract_text(file_path):
-    with pdfplumber.open(file_path) as pdf:
-        text = ""
-        for page in pdf.pages:
-            text += page.extract_text()
-    return text
-
-text = extract_text(file_path)
-
-text = (text
-        .replace('Số CT/ Doc No', 'break_point').replace('Postal', 'break_point')
-        )
-
-data2 = text.split('break_point')
-
-content_text = ''
-
-for index, content in enumerate(data2):
-    if index%2==1:
-        content_text += content
-
-for doc in set(docs):
-    content_text = content_text.replace(doc,'')
-
-for money_text in set(money_texts):
-    content_text = content_text.replace(money_text,'')
-
-for date in set(dates):
-    content_text = content_text.replace(date, 'break_point')
+def clean_text(text):
+    text = re.sub(r'Số CT/ Doc No|Postal', 'break_point', text)
+    text = text.split('break_point')
     
-content_text = content_text.replace('\n', ' ')
+    content_text = ''
+    for index, content in enumerate(text):
+        if index % 2 == 1:
+            content_text += content
+            
+    pattern = '|'.join(re.escape(doc) for doc in set(docs))  + '|' + \
+            '|'.join(re.escape(money_text) for money_text in money_texts)
+            
+    content_text = re.sub(pattern, '', content_text)
+    
+    pattern = '|'.join(re.escape(date) for date in set(dates))
+    
+    content_text = re.sub(pattern, 'break_point', content_text)
+    
+    content_text = content_text.replace('\n', ' ')
+    content_text = content_text.split('break_point')
+    content_text = [content.strip().replace('  ', ' ') for content in content_text]
+    
+    return content_text
 
-content_text = content_text.split('break_point')
-content_text = [content.strip().replace('  ', ' ') for content in content_text]
+content_text = clean_text(all_texts)
 
 print(f'Number of content: {len(content_text[1:])}')
 
