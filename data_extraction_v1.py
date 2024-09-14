9448b64055f917e4dd5d9afb6325f020c46dac02
Neo-Nguyen-95 <dzung.usth@gmail.com>
2024-09-14 10:01:05 +0700

    update first extraction

--- data_extraction_v1.py
+++ data_extraction_v1.py
@@ -2,14 +2,18 @@
 import pdfplumber
 import pandas as pd
 import re
+import time
 
-file_path = 'data/sao_ke_MTTQ_p1_3.pdf'
-#%%
+pd.set_option('display.max_columns', None)
+
+#%% LOAD DATA
+file_path = 'data/sao_ke_MTTQ_full.pdf'
+
 def extract_table(file_path):
     with pdfplumber.open(file_path) as pdf:
         all_tables = []
         all_texts = ''
-        for page in pdf.pages:
+        for page in pdf.pages[:1]:
             tables = page.extract_table()
             all_tables.extend(tables)
             
@@ -17,59 +21,73 @@ def extract_table(file_path):
             
     return all_tables, all_texts
 
+start_time = time.time()
 all_tables, all_texts = extract_table(file_path)
+end_time = time.time()
+print(f"Elapsed time: {end_time - start_time}")
 
-#%% I. EXTRACT DATE & CASH
-dates, docs, money_texts, moneys = [] , [], [], []
+#%% I. EXTRACT DATE & DOCS
+dates, docs = [], []
 
 for index, content in enumerate(all_tables):
     if index % 2 == 1:
         dates.extend(content[0].splitlines()[0::2])
         docs.extend(content[0].splitlines()[1::2])
-        money_texts.extend(content[2].splitlines())
-        moneys.extend(content[2].replace('.','').splitlines())
 
 # verification
-if (len(dates)==len(docs)) & (len(docs)==len(moneys)):
+if len(dates)==len(docs):
     print(f'Number of transaction: {len(dates)}')
-    print("Everything's fine")
+    print("Dates and cash values are valid")
+else:
+    print('Conflict date and cash values')
+    print(f'Number of date: {len(dates)}')
+    print(f'Number of docs: {len(docs)}')
 
-#%% II. EXTRACT CONTENT
+#%% II. EXTRACT MONEY & CONTENT
 def clean_text(text):
-    text = re.sub(r'Số CT/ Doc No|Postal', 'break_point', text)
+    ### Split 1: Remove header and footer in the page
+    text = re.sub(r'Số CT/ Doc No|Postal', 'break_point', text)  # detect break
     text = text.split('break_point')
     
     content_text = ''
     for index, content in enumerate(text):
         if index % 2 == 1:
             content_text += content
+    
+    ### Split 2: Separate content into pieces
+    # remove docs and moneys infor from the text
+    pattern_1 = '|'.join(re.escape(doc) for doc in set(docs))
             
-    pattern = '|'.join(re.escape(doc) for doc in set(docs))  + '|' + \
-            '|'.join(re.escape(money_text) for money_text in money_texts)
-            
-    content_text = re.sub(pattern, '', content_text)
+    content_text = re.sub(pattern_1, '', content_text)
     
-    pattern = '|'.join(re.escape(date) for date in set(dates))
+    # replace \n with space
+    content_text = re.sub(r'\n', ' ', content_text)
     
-    content_text = re.sub(pattern, 'break_point', content_text)
-    
-    content_text = content_text.replace('\n', ' ')
+    # split break point
+    pattern_2 = '|'.join(re.escape(date) for date in set(dates))
+    content_text = re.sub(pattern_2, 'break_point', content_text)
     content_text = content_text.split('break_point')
-    content_text = [content.strip().replace('  ', ' ') for content in content_text]
     
-    return content_text
+    return [content.strip().replace('  ', ' ') for content in content_text if len(content)>1]
 
 content_text = clean_text(all_texts)
 
-print(f'Number of content: {len(content_text[1:])}')
+# verification
+if len(dates) == len(content_text):
+    print(f'Number of content: {len(content_text)}')
+    print("Contents are valid")
+else:
+    print("Contents are invalid")
 
 #%% MERGE
-
 df = pd.DataFrame({'date': dates,
-                   'amount': moneys,
-                   'content': content_text[1:]})
+                   'full_content': content_text
+                    })
 
-print(df.head())
+df[['money', 'content']] = df['full_content'].str.split(' ', n=1, expand=True)
+df['money'] = df['money'].str.replace('.', '', regex=False)
+
+df.to_csv('page_1_500.csv', index=False)
 
 
 
