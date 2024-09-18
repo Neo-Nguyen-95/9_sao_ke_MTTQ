1714d7c4692ea60a271dd79d80caa32f59e700bf
Neo-Nguyen-95 <dzung.usth@gmail.com>
2024-09-18 14:32:12 +0700

    progress in Vietin data cleaning

--- module_Vietin_clean.py
+++ module_Vietin_clean.py
@@ -2,8 +2,14 @@ import os
 import pandas as pd
 
 file_list = os.listdir('data_MTTQ TW_Vietin_13.9_15.9')
+file_list.remove('.DS_Store')
 
 df = pd.DataFrame()
 for file in file_list:
+    
     df_temp = pd.read_csv('data_MTTQ TW_Vietin_13.9_15.9/' + file)
     df = pd.concat([df, df_temp], axis='rows')
+    
+df = df[df['money'].notna()]
+
+df['money'].apply(lambda row: row if row.isnumeric() else row[:-2])
