690fa2d0fc59df7132d684bffb393d2a191025b3
Neo-Nguyen-95 <dzung.usth@gmail.com>
2024-09-19 01:07:44 +0700

    data agribank

--- main_extract_Agribank.py
+++ main_extract_Agribank.py
@@ -23,53 +23,65 @@ def extract_pdf(file_path, start_page, end_page):
     return all_tables
 
 # pdf_length = 2
-start_page = 1
+start_page = 1706
+start_name = start_page
 num_page = 1
 
 df = pd.DataFrame()
-while start_page < pdf_length:
+while start_page <= pdf_length:
     end_page = min(pdf_length, start_page - 1 + num_page)
-    data = extract_pdf(file_path, start_page - 1, end_page)
-    
-    data = data[1:]
+    data = extract_pdf(file_path, start_page - 1, end_page)        
     
     dates, moneys, contents, balance = [], [], [], []
     for information in data:
-        # extract date
-        dates.append(information[0])
         
-        # extract money
-        moneys.append(information[3].split(' ')[-1].replace(',',''))
-        
-        # extract content
-        contents.append(information[1].replace('\n', ' '))
-        
-        # extract balance
-        balance.append(information[4].replace(',',''))
-    
+        try:
+            if int(information[3].replace(',','')):
+                # extract date
+                dates.append(information[0])
+                
+                # extract money
+                moneys.append(information[3].split(' ')[-1].replace(',',''))
+                
+                # extract content
+                contents.append(information[1].replace('\n', ' '))
+                
+                # extract balance
+                balance.append(information[4].replace(',',''))
+        except:
+            pass
     df_temp = pd.DataFrame({
         'date': dates,
         'money': moneys,
         'content': contents,
         'balance': balance
         })
-
-    df = pd.concat([df, df_temp], axis='rows')
+    
     
     # check
-    # if df_temp['money'].astype(int).sum() == (
-    #         df_temp['balance'].astype(int).iloc[-1] - df_temp['balance'].astype(int).iloc[0]):
-    #     df = pd.concat([df, df_temp], axis='rows')
-    # else: 
-    #     print(f'Stop at: {end_page}')
-    
-    if len(df)%200 == 0:
-        df.to_csv('page_' + str(start_page) + '_' + str(end_page) + '.csv',
+    try:
+        print(f'Now at page {end_page}')
+        if df_temp['money'][1:].astype(int).sum() == (
+                df_temp['balance'].astype(int).iloc[-1] - df_temp['balance'].astype(int).iloc[0]):
+            df = pd.concat([df, df_temp], axis='rows')
+            
+            if end_page%200 == 0:
+                df.to_csv('page_' + str(start_name) + '_' + str(end_page) + '.csv',
+                          index=False)
+            
+            # update start page
+            start_page += num_page
+        else:
+            df.to_csv('page_' + str(start_name) + '_' + str(end_page-1) + '.csv',
+                      index=False)
+            break
+    except:
+        df.to_csv('page_' + str(start_name) + '_' + str(end_page-1) + '.csv',
                   index=False)
+        print(f'Stop at: {end_page}')
+        start_page = pdf_length + 1
+        break
     
-    
-    # update start page
-    start_page += num_page
+pd.DataFrame(data).to_csv('page_' + str(end_page) + '.csv', index=False)  
 
-df.to_csv('page_' + str(start_page) + '_' + str(end_page) + '.csv',
-          index=False)
+
